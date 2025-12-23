58928608060a4ed7f7892dccd5e8cac1689ff8f5
Tahazzat <tahazzat@HP.lan>
2025-12-23 19:21:15 +0600

    converted to v3

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,14 +1,22 @@
+// tailwind.config.ts
 import type { Config } from "tailwindcss";
 
 const config: Config = {
-    darkMode: ["class", "[data-theme='dark']"],
+    darkMode: "class",
     content: [
-        "./app/**/*.{ts,tsx}",
-        "./components/**/*.{ts,tsx}",
-        "./sharedComponents/**/*.{ts,tsx}",
+        "./src/app/**/*.{js,ts,jsx,tsx,mdx}",
+        "./src/components/**/*.{js,ts,jsx,tsx,mdx}",
+        "./src/sharedComponents/**/*.{js,ts,jsx,tsx,mdx}",
     ],
     theme: {
-        extend: {},
+        extend: {
+            colors: {
+                primary: "var(--primary)",
+                secondary: "var(--secondary)",
+                background: "var(--background)",
+                foreground: "var(--foreground)",
+            },
+        },
     },
     plugins: [],
 };
