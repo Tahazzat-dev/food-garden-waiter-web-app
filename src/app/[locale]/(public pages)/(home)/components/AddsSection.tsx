46009bea4d29a3a9e713d4e8e7193f6f4f710303
= <=>
2025-12-28 12:23:54 +0600

    changes padding of ads element

--- src/app/[locale]/(public pages)/(home)/components/AddsSection.tsx
+++ src/app/[locale]/(public pages)/(home)/components/AddsSection.tsx
@@ -6,7 +6,7 @@ export default function AddsSection() {
     const t = useTranslations('shared');
     return (
         <section className='w-full' >
-            <Container className='mt-4'>
+            <Container className='lg:mt-4'>
                 <div className="w-full flex items-center justify-center min-h-[100px] md:min-h-[200px] bg-slate-400 dark:bg-slate-600 rounded-md lg:rounded-xl relative">
 
                     <h5 className='text-center'>{t('ads')}</h5>
