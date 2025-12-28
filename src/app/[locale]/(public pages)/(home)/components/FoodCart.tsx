5157f8281d5b059cc2ac625b5e8c8489977544d5
= <=>
2025-12-28 13:27:39 +0600

    changes icon width and height

--- src/app/[locale]/(public pages)/(home)/components/FoodCart.tsx
+++ src/app/[locale]/(public pages)/(home)/components/FoodCart.tsx
@@ -54,8 +54,8 @@ export default function FoodCart({ product }: { product: TProduct }) {
                     <div className="w-full h-full overflow-hidden">
                         <Image src={product?.img} className='z-10 w-full duration-300 group-hover:scale-105 h-full' width={300} height={400} alt={locale === "bn" ? product?.title.bn : product?.title.en || 'Product Image'} />
                     </div>
-                    <Link className='font-semibold p-0.5 px-1 custom-shadow-md bg-primary hover:bg-primary-500 text-white absolute bottom-1 md:bottom-2 right-1 md:right-2 z-20' href={`/products/${product.id}`}>
-                        <Eye className='' />
+                    <Link className='font-semibold p-1 px-[5.5px] custom-shadow-md bg-primary hover:bg-primary-500 text-white absolute bottom-1 md:bottom-2 right-1 md:right-2 z-20' href={`/products/${product.id}`}>
+                        <Eye className='w-5 h-5' />
                     </Link>
                     {
                         !!product?.discount && <span className='absolute fg_fs-xxs  top-2 right-2 bg-secondary text-white px-2 py-1 rounded-md'>
@@ -67,7 +67,7 @@ export default function FoodCart({ product }: { product: TProduct }) {
                 <div className="w-full p-3 md:p-4 bg-slate-100 dark:bg-slate-700">
                     <h6 className='mb-1'>{locale === "bn" ? product?.title.bn : product?.title.en}</h6>
                     <p className='fg_fs-sm'>{product.discount < 1 ? <span className=''>{product?.price}TK</span> : <span className='flex items-center gap-3'> <span className='line-through fg_fs-xs'>{product?.price}TK</span> <span className='text-primary'>{discountedPrice}TK</span></span>}</p>
-                    <Button onClick={openDetailsModal} className={`text-white w-full font-semibold  ${isAddedToCart ? ' bg-secondary hover:bg-secondary !cursor-not-allowed' : 'custom-shadow-md  bg-primary hover:bg-primary-500'}`} ><ShoppingCart /> <span>{t('addToCart')}</span></Button>
+                    <Button onClick={openDetailsModal} className={`mt-2 text-white w-full font-semibold  ${isAddedToCart ? ' bg-secondary hover:bg-secondary !cursor-not-allowed' : 'custom-shadow-md  bg-primary hover:bg-primary-500'}`} ><ShoppingCart /> <span>{t('addToCart')}</span></Button>
                 </div>
             </Link>
 
