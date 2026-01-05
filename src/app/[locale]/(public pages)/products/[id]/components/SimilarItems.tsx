b55bbf137f10d125d5629e753a6e8c11b3ba37d6
= <=>
2026-01-05 12:49:32 +0600

    added title font size of similar products

--- src/app/[locale]/(public pages)/products/[id]/components/SimilarItems.tsx
+++ src/app/[locale]/(public pages)/products/[id]/components/SimilarItems.tsx
@@ -5,9 +5,9 @@ import { useTranslations } from 'next-intl'
 export default function SimilarItems({ items }: { items: TProduct[] }) {
     const t = useTranslations('shared')
     return (
-        <div className='mt-12 md:mt-14 lg:mt-16 xl:mt-20'>
+        <div className='mt-12 md:mt-14 lg:mt-16 '>
             <Container className="w-full">
-                <h5 className='mb-4 md:mb-5'>{t("similarItems")}</h5>
+                <h5 className='mb-4 fg_fs-lg font-semibold'>{t("similarItems")}</h5>
                 <div className="w-full grid gap-2.5 sm:gap-4 grid-cols-2 md:grid-cols-[repeat(auto-fill,minmax(250px,1fr))]">
                     {
                         items.map(product => <FoodCart product={product} key={product?.id} />)
