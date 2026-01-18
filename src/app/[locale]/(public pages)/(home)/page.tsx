0a66489d59cc3027f41c5bed74da32f6c3476404
= <=>
2026-01-18 15:13:01 +0600

    removed meta data from home page

--- src/app/[locale]/(public pages)/(home)/page.tsx
+++ src/app/[locale]/(public pages)/(home)/page.tsx
@@ -3,34 +3,8 @@ import MobileSearch from "./components/MobileSearch";
 // import AddsSection from "./components/AddsSection";
 import FilterFood from "./components/FilterFood";
 import CategorySection from "@/sharedComponents/category/CategoryContainer";
-import { Metadata } from "next";
 
 export const revalidate = 300  // revalidate in every 5 minutes
-
-
-
-// app/[locale]/layout.tsx
-export const metadata: Metadata = {
-    metadataBase: new URL('https://foodgardencafe.com'),
-    title: {
-        default: 'Food Garden – Fresh & Delicious',
-        template: '%s | Food Garden'
-    },
-    description: 'An online food delivery platform bringing fresh and delicious meals to your doorstep.',
-    alternates: {
-        canonical: 'https://foodgardencafe.com',
-    },
-    openGraph: {
-        type: 'website',
-        siteName: 'Food Garden',
-        locale: 'bn_BD',
-        images: ['/og-layout.png']
-    },
-    twitter: {
-        card: 'summary_large_image'
-    }
-};
-
 
 export default function HomePage() {
     return (
