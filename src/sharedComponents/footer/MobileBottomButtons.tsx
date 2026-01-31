62d5fce509316c38a62c5b1c6db0b22014fa5b7f
Tahazzat-dev <=>
2026-01-31 08:25:14 +0600

    changes mobile bootom icons

--- src/sharedComponents/footer/MobileBottomButtons.tsx
+++ src/sharedComponents/footer/MobileBottomButtons.tsx
@@ -1,16 +1,16 @@
 "use client"
-import { ShoppingCart } from 'lucide-react'
-import Image from 'next/image'
 import { Link } from "@/i18n/navigation";
-import Container from '../wrapper/Container'
-import { useDispatch, useSelector } from 'react-redux'
-import { RootState } from '@/redux/store'
-import { SET_EXPAND, toggleRunExpandAnimation, updateCartIconPosition } from '@/redux/features/actions/actionSlice'
-import { useTranslations } from 'next-intl'
-import { useEffect, useRef } from 'react'
-import { cn, hasVisitedOffersToday } from '@/lib/utils'
-import { setShowOfferedMark } from '@/redux/features/product/productSlice'
-import RenderText from '../utils/RenderText'
+import { cn, hasVisitedOffersToday } from '@/lib/utils';
+import { SET_EXPAND, toggleRunExpandAnimation, updateCartIconPosition } from '@/redux/features/actions/actionSlice';
+import { setShowOfferedMark } from '@/redux/features/product/productSlice';
+import { RootState } from '@/redux/store';
+import { Globe, ShoppingCart } from 'lucide-react';
+import { useTranslations } from 'next-intl';
+import Image from 'next/image';
+import { useEffect, useRef } from 'react';
+import { useDispatch, useSelector } from 'react-redux';
+import RenderText from '../utils/RenderText';
+import Container from '../wrapper/Container';
 
 export default function MobileBottomButtons() {
     // variables
@@ -68,18 +68,12 @@ export default function MobileBottomButtons() {
         <div className='w-full dark:border-t dark:border-slate-400 md:hidden bg-black fixed py-2 z-[999999] bottom-0 left-0'>
             <Container className='flex justify-between'>
                 <Link className='flex flex-col gap-1 items-center justify-between min-w-[70px]' href="/" >
-                    <Image src="/images/shared/home-icon.svg" width={22} height={40} alt="Home icon" />
-                    <span className='text-white font-semibold text-sm sm:text-base'><RenderText group="mobileBottomActions" variable="home" /></span>
+                    <Image src="/images/shared/food-menu-icon.svg" width={22} height={40} alt="Menu icon" />
+                    <span className='text-white font-semibold text-sm sm:text-base'><RenderText group="mobileBottomActions" variable="menu" /></span>
                 </Link>
                 <Link href="/offers" className='flex flex-col gap-1 items-center justify-between min-w-[70px]' >
-                    <div className='relative'>
-                        <Image src="/images/shared/special.svg" width={25} height={40} alt="Offer icon" />
-                        {
-                            !!showOfferedMark &&
-                            <span className='flex items-center justify-center text-xs px-0.5 min-w-[18px] min-h-4  absolute -top-[40%] left-[80%] translate-x-[-50%] bg-secondary text-white rounded-full p-[1px]'>!</span>
-                        }
-                    </div>
-                    <span className='text-white font-semibold text-sm sm:text-base'><RenderText group="mobileBottomActions" variable="offers" /></span>
+                    <Image src="/images/shared/orders-icon.svg" width={25} height={40} alt="Orders icon" />
+                    <span className='text-white font-semibold text-sm sm:text-base'><RenderText group="mobileBottomActions" variable="orders" /></span>
                 </Link>
                 <button onClick={() => dispatch(SET_EXPAND(EXPAND === KEY ? null : KEY))} className='min-w-[70px] prevent-body-trigger relative flex flex-col gap-1 items-center justify-between' >
                     <div className={cn('relative', runExpandAnimation && "cart-pop")}>
@@ -93,19 +87,13 @@ export default function MobileBottomButtons() {
                 </button>
 
                 <Link href="/orders" className='relative flex flex-col gap-1 items-center justify-between min-w-[70px]' >
-                    <div className='relative'>
-                        <Image src="/images/shared/my-order.svg" width={25} height={40} alt="Orders icon" />
-                        {
-                            pendingOrders.length > 0 &&
-                            <span className='flex items-center justify-center text-xs px-0.5 min-w-[18px] min-h-4  absolute -top-[40%] left-[80%] translate-x-[-50%] bg-secondary text-white rounded-full p-[1px]'>{pendingOrders.length}</span>
-                        }
-                    </div>
-                    <span className='text-white font-semibold text-sm sm:text-base'><RenderText group="mobileBottomActions" variable="orders" /></span>
+                    <Image src="/images/shared/cooking-icon.svg" width={25} height={40} alt="Orders icon" />
+                    <span className='text-white font-semibold text-sm sm:text-base'><RenderText group="mobileBottomActions" variable="kitchen" /></span>
                 </Link>
 
                 <Link target='_blank' href="https://wa.me/8801713619293" className='flex flex-col gap-1 items-center justify-between min-w-[70px]' >
-                    <Image src="/images/shared/whatsapp.svg" width={25} height={40} alt="Whatsapp icon" />
-                    <span className='text-white font-semibold text-sm sm:text-base'><RenderText group="shared" variable="whatsapp" /></span>
+                    <Globe className='text-white h-6 w-6 cursor-pointer' />
+                    <span className='text-white font-semibold text-sm sm:text-base'><RenderText group="mobileBottomActions" variable="online" /></span>
                 </Link>
             </Container>
         </div>
