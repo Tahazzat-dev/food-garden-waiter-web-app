290906acce64bd1e155ab340b3bbaa9aba699d5e
Tahazzat-dev <tahazzat.dev@gmail.com>
2026-02-06 07:43:05 +0600

    added proxy for protected route'

--- src/proxy.ts
+++ src/proxy.ts
@@ -1,8 +1,43 @@
 import createMiddleware from 'next-intl/middleware';
+import { NextRequest, NextResponse } from 'next/server';
 import { routing } from './i18n/routing';
 
-export default createMiddleware({ ...routing, localeDetection: false });
+// create next-intl middleware instance
+const intlMiddleware = createMiddleware({
+    ...routing,
+    localeDetection: false
+});
+
+export default function middleware(request: NextRequest) {
+    const { pathname } = request.nextUrl;
+
+    // get token from cookie
+    const token = request.cookies.get('auth_token')?.value;
+
+    // define your routes
+    const isAuthPage = pathname.includes('/login');
+    // const isProtectedRoute =
+    //     pathname.includes('/dashboard') ||
+    //     pathname.includes('/profile');
+
+    // no token → redirect to login
+    if (!token && !isAuthPage) {
+        const url = request.nextUrl.clone();
+        url.pathname = '/login';
+        return NextResponse.redirect(url);
+    }
+
+    // already logged in → prevent login page
+    if (token && isAuthPage) {
+        const url = request.nextUrl.clone();
+        url.pathname = '/dashboard';
+        return NextResponse.redirect(url);
+    }
+
+    // run next-intl middleware normally
+    return intlMiddleware(request);
+}
 
 export const config = {
     matcher: '/((?!api|trpc|_next|_vercel|.*\\..*).*)'
-}
+};
---
+++ src/sharedComponents/modal/OrderSuccessModal.tsx
@@ -0,0 +1,43 @@
+"use client";
+import { Button } from "@/components/ui/button";
+import * as Dialog from "@radix-ui/react-dialog";
+import { ShoppingCart, X } from "lucide-react"; // optional icon
+
+
+
+export default function OrderSuccessModal() {
+    // variables
+    const KEY = "OPEN_PRODUCT_DETAILS_MODAL";
+
+
+
+    if (!modalProduct) return null;
+
+    return (
+        <>
+            <Dialog.Root open={KEY === EXPAND} onOpenChange={closeModal}>
+                <Dialog.Portal>
+                    <div className="fixed inset-0 global-overlay z-[9999]" />
+                    <Dialog.Content className="prevent-body-trigger fixed top-1/2 left-1/2  max-w-[93vw] md:max-w-[700px] !rounded-[10px] lg:!rounded-[12px] overflow-hidden w-full -translate-x-1/2 -translate-y-1/2 bg-body rounded-lg shadow-lg dark:shadow-slate-800 z-[99999]">
+                        <div className="flex items-center justify-between bg-primary px-4 py-2">
+                            <Dialog.Title className="fg_fs-md text-white">
+                                {t('foodDetails')}
+                            </Dialog.Title>
+                            <Button onClick={closeModal} className="rounded-full !px-2.5" variant="secondary"> <X className="!text-white w-5 md:w-6 md:h-6 h-5 lg:w-8 lg:h-8" /></Button>
+                        </div>
+                        <div className="p-4">
+
+                        </div>
+                        <div className="w-full flex justify-end p-4">
+                            <Dialog.Close className="" asChild >
+                                <Button disabled={showVariantWarning} onClick={handleAddToCart} className={`text-white mt-2 font-semibold  ${addedItem && addedItem.quantity === quantity ? ' bg-secondary hover:bg-secondary' : 'custom-shadow-md  bg-primary hover:bg-primary-500'}`} ><ShoppingCart /> <span>{t('addToCart')}</span></Button>
+                            </Dialog.Close>
+                        </div>
+                    </Dialog.Content>
+                </Dialog.Portal>
+            </Dialog.Root>
+        </>
+    )
+}
+
+
